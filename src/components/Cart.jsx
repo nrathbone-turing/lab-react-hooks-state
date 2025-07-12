ecdc3c35381bfd596ddb212642488a7734263807
Nick Rathbone <nrathbone@joinhandshake.com>
2025-07-12 13:40:43 -0600

    create a ul element with li tags for each item name in the cart array

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,9 +4,12 @@ const Cart = () => {
   return (
     <div>
       <h2>Shopping Cart</h2>
+      {/* Include items here in li tags with text 'ITEM.NAME is in your cart.' */}
       <ul>
-        {/* TODO: Include items here in li tags with text 'ITEM.NAME is in your cart.' */}
-      </ul>
+      {cartItems.map(item => (
+        <li key={item.id}>{item.name} is in your cart.</li>
+      ))}
+    </ul>
     </div>
   )
 }
